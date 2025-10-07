69082fc15b8177c176a4c4c2a99c679063e2d101
unknown <amoshal1997@gmail.com>
2025-10-07 17:04:11 +0300

    save users

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,15 @@
 const TelegramBot = require("node-telegram-bot-api");
 const { BOT_TOKEN } = require("./config");
+const { loadSessions, saveSessions } = require("./utils/sessionStorage");
 
-const userSessions = {};
+const userSessions = loadSessions();
 let bot;
 
 function initBot() {
   if (!bot) {
     bot = new TelegramBot(BOT_TOKEN, { polling: true });
+
+    setInterval(() => saveSessions(userSessions), 30_000);
   }
   return bot;
 }
